97cbe0c04273e00f1bac7080650d170d26292a32
hacktar <ugo@inzhoop.com>
2016-11-08 11:28:41 +0100

    compact wallet and address to on sliding page

--- www/js/controllers/ctrlWallet.js
+++ www/js/controllers/ctrlWallet.js
@@ -1,9 +1,23 @@
 angular.module('leth.controllers')
   .controller('WalletCtrl', function ($scope, $stateParams, $ionicLoading, $ionicModal, $state, 
-                                      $ionicPopup, $cordovaBarcodeScanner, $ionicActionSheet, 
-                                      $timeout, AppService, Transactions,ExchangeService, Chat) {
+                                      $ionicPopup, $cordovaBarcodeScanner, $ionicSlideBoxDelegate, $ionicActionSheet, 
+                                      $timeout, $cordovaEmailComposer, $cordovaClipboard, $cordovaSms,
+                                      AppService, Transactions,ExchangeService, Chat) {
     var TrueException = {};
     var FalseException = {};
+
+    $scope.size = 250;
+    $scope.correctionLevel = 'H';
+    $scope.typeNumber = 12;
+    $scope.inputMode = '';
+    $scope.image = true;
+    $scope.qrcodeString = AppService.account() + "#" + AppService.idkey();
+    
+    $scope.listUnit = [
+      {multiplier: "1.0e18", unitName: "Ether"},
+      {multiplier: "1.0e15", unitName: "Finney"},
+      {multiplier: "1", unitName: "Wei"}
+    ];
 
     var setCoin = function(index){
       if(index==0){
@@ -70,6 +84,10 @@ angular.module('leth.controllers')
       $scope.fromAddressBook = true;
     }else { 
       $scope.fromAddressBook = false;
+    }
+
+    $scope.listTransaction = function(){
+      $state.go('app.transactions');
     }
 
     $scope.sendCoins = function (addr, amount, unit, idCoin) {
@@ -216,7 +234,100 @@ angular.module('leth.controllers')
       })
     };
 
-    $scope.listTransaction = function(){
-      $state.go('app.transactions');
+    $scope.onAmountChange = function(amount){
+      if($scope.amountPayment == "")
+        $scope.qrcodeString = AppService.account() + "#" + AppService.idkey();
+      $scope.qrcodeString = AppService.account() + "#" + AppService.idkey() + '@' + amount;
+    }
+
+    $scope.showAddress = function () {
+      var alertPopup = $ionicPopup.alert({
+        title: 'Wallet Address',
+        template: $scope.qrcodeString
+        //template: "<div class='item text-center'><qr text='" + $scope.qrcodeString + "' type-number='typeNumber' correction-level='correctionLevel' size='300' input-mode='inputMode' image='image'></qr></div>"
+      });
+
+      alertPopup.then(function(res) {
+        console.log('show address');
+      });
+    };
+
+    $scope.shareBySms = function() {
+      var content = "My address is ethereum://" + $scope.qrcodeString ;
+      var phonenumber="";
+      document.addEventListener("deviceready", function () {      
+        $cordovaContacts.pickContact().then(function (contactPicked) {
+            phonenumber = contactPicked.phoneNumbers[0].value;
+
+            var options = {
+              replaceLineBreaks: false, // true to replace \n by a new line, false by default
+              android: {
+                  intent: 'INTENT'  // send SMS with the native android SMS messaging
+                  //intent: '' // send SMS without open any other app
+              }
+            };
+
+            $cordovaSms
+            .send(phonenumber, content, options)
+            .then(function() {
+              // Success! SMS was sent
+              console.log("SMS to " + phonenumber + " with text :" + content + " sent.");
+            }, function(error) {
+              // An error occurred
+              console.log("ERROR sending SMS to " + phonenumber + " with text :" + content + " sent.");
+            });
+
+          });
+      }, false);      
+    }
+
+    $scope.shareByEmail = function(){
+      var imgQrcode = angular.element(document.querySelector('qr > img')).attr('ng-src');
+      //I need to remove header of bitestream and replace with the new one
+      var allegato = 'base64:qr.png//'+imgQrcode.replace('data:image/png;base64,','');
+     
+      document.addEventListener("deviceready", function () {
+        $cordovaEmailComposer.isAvailable().then(function() {
+        
+          var emailOpts = {
+            to: [''],
+            attachments:[allegato],
+            subject: 'Please Pay me',
+            body: '<h3>Please send me ETH to this Wallet Address:</h3> <p><a href="ethereum://' + $scope.qrcodeString + '">ethereum://' + $scope.qrcodeString + '</a></p>',
+            isHtml: true
+          };
+
+          $cordovaEmailComposer.open(emailOpts).then(null, function () {
+            console.log('email view dismissed');
+          });
+
+          return;
+        }, function (error) {
+          console.log("cordovaEmailComposer not available");
+          return;
+        });
+      }, false);         
+    }
+
+    $scope.copyAddr = function(){
+      document.addEventListener("deviceready", function () {  
+        $cordovaClipboard
+        .copy($scope.qrcodeString)
+        .then(function () {
+          // success
+          //alert('Address in clipboard');
+          var alertPopup = $ionicPopup.alert({
+             title: 'Copy Address',
+             template: 'Address is in clipboard!'
+           });
+
+           alertPopup.then(function(res) {
+             console.log('address copied in clipboard');
+           });
+        }, function () {
+          // error
+          console.log('Copy error');
+        });
+      }, false);         
     }
   })
